d979bcb9c074458bba8f96bf3ec87c7f630a3456
BeejLuig <bjcantlupe@gmail.com>
2019-10-15 22:40:53 -0400

    add draft logic

--- src/pages/index.js
+++ src/pages/index.js
@@ -57,7 +57,10 @@ export const pageQuery = graphql`
         }
       }
     }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+    allMarkdownRemark(
+      filter: { fields: { draft: { eq: false } } }
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
       edges {
         node {
           excerpt
